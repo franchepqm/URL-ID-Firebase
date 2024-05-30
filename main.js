a2ba4446badac7c54c18f0c6b8a74324a9234c82
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-05-30 14:49:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ async function cargarDetalleItem() {
     const itemId = urlParams.get('itemId');
     const detalleItemContainer = document.getElementById('detalle-item');
 
-    if (!itemId) {
-        detalleItemContainer.innerHTML = "<p>Error: No se proporcionó un ID de item válido.</p>";
-        return;
-    }
-
     try {
         const docRef = doc(firestore, "items", itemId);
         const docSnap = await getDoc(docRef);
@@ -16,15 +11,11 @@ async function cargarDetalleItem() {
             const data = docSnap.data();
             detalleItemContainer.innerHTML = `
                 <h2>${data.title}</h2>
-                <p><strong>Categoría:</strong> ${data.category}</p>
                 <p><strong>Descripción:</strong> ${data.description}</p>
-                <p><strong>Fecha y hora:</strong> ${data.timestamp.toDate().toLocaleString()}</p>
             `;
-        } else {
-            detalleItemContainer.innerHTML = "<p>No se encontró el documento.</p>";
-        }
+        } 
+        else{}
     } catch (error) {
         console.log("Error al cargar el detalle del item: ", error);
-        detalleItemContainer.innerHTML = "<p>Error al cargar los detalles del item.</p>";
     }
 }
