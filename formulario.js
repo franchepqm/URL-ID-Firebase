696e37c54042abf6bb65660ac701ef67d2fd7bf9
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-05-30 14:50:18 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,7 +1,6 @@
 document.getElementById('admin-form').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const category = document.getElementById('category').value;
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
 
@@ -9,15 +8,12 @@ document.getElementById('admin-form').addEventListener('submit', async function(
         const docRef = doc(firestore, "items", title);
 
         await setDoc(docRef, {
-            category: category,
             title: title,
             description: description,
-            timestamp: new Date()
         });
 
         alert("Documento añadido con éxito");
     } catch (error) {
         console.error("Error al agregar datos", error);
-        alert("Error al añadir documento");
     }
 });
